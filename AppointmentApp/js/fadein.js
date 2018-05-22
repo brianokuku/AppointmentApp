809f8eaedc81dbca07c096f2c89f054f53eef9b3
brianokuku <35316876+brianokuku@users.noreply.github.com>
2018-05-21 20:12:29 -0500

    Add files via upload

--- AppointmentApp/js/fadein.js
+++ AppointmentApp/js/fadein.js
@@ -1,8 +1,11 @@
+
+////JQuery to display the new form
 $('#new_button').click(function(){
 $('#new_form').slideToggle(500);
 $(this).hide();
 });
 
+//JQuery to hide the form
 $('#cancel_button').click(function(){
 $('#new_form').slideToggle(500);
 $('#new_button').show();
